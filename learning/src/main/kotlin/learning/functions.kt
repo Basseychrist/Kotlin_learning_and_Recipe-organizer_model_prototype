d3f0875ffa20c626555a5718de74fa4d806eb9e2
basseychrist <enobongetimbassey101@gmail.com>
2026-07-16 12:40:20 +0100

    renamed folder

--- learning/src/main/kotlin/learning/functions.kt
+++ learning/src/main/kotlin/learning/functions.kt
@@ -10,7 +10,7 @@ fun square(number: Int) = number * number
 
 // Shows how to call both a normal function and a single-expression function.
 fun main() {
-    val greeting = greetUser("Developer")
+    val greeting = greetUser("Bassey")
     println(greeting)
 
     val result = square(5)
